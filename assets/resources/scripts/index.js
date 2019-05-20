5b10a458b2d60d0a231e2ed645e77763f706ab32
bruw <brunojwogt@gmail.com>
2019-05-20 16:00:43 -0300

    Botões incrementando e decrementando

--- assets/resources/scripts/index.js
+++ assets/resources/scripts/index.js
@@ -28,5 +28,60 @@
         $('#card-content-drink3').slideToggle('fast');
     })
 
+    //variáveis de controle do pedido
+    let cont_hamb1 = 0;
+    let cont_hamb2 = 0;
+
+    //incrementa a quantidade no pedido - tipo do hamburguer tradicional
+    //verifica se atingiu o limite do pedido (6 unidades) do mesmo tipo de hamburguer
+    $('#button-add-unit-hamb1').click(function(){
+        cont_hamb1++;
+        if(cont_hamb1 <= 6){
+            $('#card-content-hamb1 > +p').text('Seu pedido: '+cont_hamb1);
+            $('#button-buy-item').css('display', 'block');
+
+        }
+        else{
+            cont_hamb1 = 6;
+            window.alert('Limite máximo atingido... (6 unidades)')
+        }
+    })
+
+    //decrementa a quantidade exibida no card do produto - tipo do hamburguer tradicional
+    $('#button-remove-unit-hamb1').click(function(){
+        cont_hamb1--;
+        if(cont_hamb1 >= 0){
+            $('#card-content-hamb1 > +p').text('Seu pedido: '+cont_hamb1);
+        }else{
+            $('#button-buy-item').css('display', 'none');
+            cont_hamb1= 0;
+            window.alert('Você não possui nenhuma unidade no seu pedido')
+        }
+    })
+    //incrementa a quantidade no pedido - tipo do chesse burguer
+    //verifica se atingiu o limite do pedido (6 unidades) do mesmo tipo de hamburguer
+    $('#button-add-unit-hamb2').click(function(){
+        cont_hamb2++;
+        if(cont_hamb2 <= 6){
+            $('#card-content-hamb2 > +p').text('Seu pedido: '+cont_hamb2);
+        }
+        else{
+            cont_hamb2 = 6;
+            window.alert('Limite máximo atingido... (6 unidades)')
+        }
+    })
+    
+    //decrementa a quantidade exibida no card do produto - tipo do hamburguer tradicional
+    $('#button-remove-unit-hamb2').click(function(){
+        cont_hamb2--;
+        if(cont_hamb2 >= 0){
+            $('#card-content-hamb2 > +p').text('Seu pedido: '+cont_hamb2);
+        }
+        else{
+            cont_hamb2 = 0;
+            window.alert('Você não possui nenhuma unidade no seu pedido')
+        }
+    })
+
    }); 
 }();
